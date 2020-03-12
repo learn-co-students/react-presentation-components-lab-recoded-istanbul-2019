39a03b715bce605d62125f912318bcec948aa370
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2020-03-13 02:20:16 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+let handleClicka=()=>{
+ console.log('hey')
+}
 ReactDOM.render(
   <div>
+
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={handleClicka} />
   </div>,
   document.getElementById('root')
 );
